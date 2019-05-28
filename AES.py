6c0e5babccdf77101a1359f70b92ee62f1a75e60
yopiyama <yoshiyama.r24@gmail.com>
2019-05-29 03:06:59 +0900

    Fix AES.py (コードがクソだったから書き直す)

--- AES.py
+++ AES.py
@@ -6,7 +6,6 @@ AES (Rijndael)
 Author : Yopiyama
 """
 
-import copy
 
 class AES:
     """
@@ -18,13 +17,16 @@ class AES:
         State Array
     """
 
-    __state_array = [[]]
-    __sbox = {"a":"a"}
+    "Nk : Key length, Nb : Block size, Nr : Number of round"
+    KEY_128 = {"bit": 128, "Nk": 4, "Nb": 4, "Nr": 10}
+    KEY_192 = {"bit": 192, "Nk": 6, "Nb": 4, "Nr": 12}
+    KEY_256 = {"bit": 256, "Nk": 8, "Nb": 4, "Nr": 14}
 
-    def __init__(self):
-        self.bit = 256
-        self.round = 14
-        self.__state_array = [["" for i in range(4)] for i in range(4)]
+    __sbox = {"a": "a"}
+
+    def __init__(self, key):
+        self.key = key
+        self.__state_array = [[0 for i in range(4)] for i in range(4)]
         # print("__init__")
 
     def __sub_bytes(self):
@@ -34,50 +36,22 @@ class AES:
     def __shift_rows(self):
         """ ShiftRows Method """
         # print("ShiftRows")
-        for i in range(4):
-            self.__state_array[i] = self.__state_array[i][i:4] + self.__state_array[i][:i]
 
     def __mix_columns(self):
         """ MixColumns Method """
         # print("MixColumns")
-        def gfm(a, b):
-            """ Multiplication on Galois Field
-            https://www.samiam.org/galois.html
-            """
-            # print("Galois Field Multiplication")
-            p = 0
-            for i in range(8):
-                if b & 1:
-                    p ^= a
-                high_bit = a & 0x80
-                a <<= 1
-                a &= 0xFF
-                if high_bit == 0x80:
-                    a ^= 0x1B
-                b >>= 1
-            return p
-
-        tmp = copy.deepcopy(self.__state_array)
-        for i in range(4):
-            self.__state_array[0][i] = gfm(tmp[0][i], 2) ^ gfm(tmp[1][i], 3) ^ tmp[2][i] ^ tmp[3][i]
-            self.__state_array[1][i] = tmp[0][i] ^ gfm(tmp[1][i], 2) ^ gfm(tmp[2][i], 3) ^ tmp[3][i]
-            self.__state_array[2][i] = tmp[0][i] ^ tmp[1][i] ^ gfm(tmp[2][i], 2) ^ gfm(tmp[3][i], 3)
-            self.__state_array[3][i] = gfm(tmp[0][i], 3) ^ tmp[1][i] ^ tmp[2][i] ^ gfm(tmp[3][i], 2)
 
     def __add_round_key(self):
         """ AddRoundKey Method """
         # print("AddRoundKey")
 
-    def __split_data(self, data):
-        """ Divide the data into 16 bytes for each 8 bits """
-        # print("Split Data")
-
     def encode(self, plain_data):
         """ Encode Method """
-        cipher_text = ""
+
         print("encode")
-        for i in range(self.round):
-            self.__split_data(plain_data)
+        print(plain_data)
+        cipher_text = ""
+        for i in range(self.key["round"] - 1):
             self.__sub_bytes()
             self.__shift_rows()
             self.__mix_columns()
@@ -87,11 +61,13 @@ class AES:
         self.__add_round_key()
         return cipher_text
 
+
 def main():
     """ Main Function """
-    aes = AES()
-    m = "a"
-    c = aes.encode(m)
+    aes = AES(AES.KEY_256)
+    c = aes.encode("m")
+    print(c)
+
 
 if __name__ == '__main__':
     main()
